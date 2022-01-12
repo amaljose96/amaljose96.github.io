d326467ea5c39d509dc3b7e717072d59320d3b04
Amal Jose <amaljose96@gmail.com>
2022-01-11 17:56:52 -0800

    Pushing Portfolio

---
+++ precache-manifest.7cb528f21e060deee8abaa57ce2afe44.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02b2e36c9d09250965cfb915f0349ecd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a52e57e333637c1e97ea",
+    "url": "/static/js/2.8ff9ee51.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.8ff9ee51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61cf05126206e7643c26",
+    "url": "/static/js/main.feb635bf.chunk.js"
+  },
+  {
+    "revision": "22cdfb1694dcdf566247",
+    "url": "/static/js/runtime-main.362843b0.js"
+  }
+]);
