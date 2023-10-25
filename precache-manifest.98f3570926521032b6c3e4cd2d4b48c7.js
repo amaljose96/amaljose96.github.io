f68a264f36d72f9d8171ac2e28f27f477b924a73
Amal Jose <amaljose96@gmail.com>
2023-10-25 11:57:42 -0700

    Amal Jose, Updated with Spotnana info

---
+++ precache-manifest.98f3570926521032b6c3e4cd2d4b48c7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c508986cef1bf88d6e2f4210366b72b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "74c956f052d4f38e3b7f",
+    "url": "/static/js/2.05121e39.chunk.js"
+  },
+  {
+    "revision": "54fd6816419f7824e2804745930c9117",
+    "url": "/static/js/2.05121e39.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "843f2f206174dd1bc990",
+    "url": "/static/js/3.2d0dfa3f.chunk.js"
+  },
+  {
+    "revision": "1a52c89f627741def720",
+    "url": "/static/js/main.770b8433.chunk.js"
+  },
+  {
+    "revision": "657720e26f794955407e",
+    "url": "/static/js/runtime-main.d9a73855.js"
+  }
+]);
